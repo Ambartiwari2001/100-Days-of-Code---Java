107d0936053033d8f9166ee7550f14337fdde34d
Ambar Tiwari <ambartiwariji1995@gmail.com>
2025-06-23 09:31:35 +0530

    Check Character is Vowel or Consonant

---
+++ Day03/VowelConsonant.java
@@ -0,0 +1,22 @@
+package Day03;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().toLowerCase().charAt(0); // lowercase me convert kar raha
+
+        if (Character.isLetter(ch)) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.println(ch + " is a vowel.");
+            } else {
+                System.out.println(ch + " is a consonant.");
+            }
+        } else {
+            System.out.println("Invalid input. Please enter an alphabet letter.");
+        }
+    }
+}
