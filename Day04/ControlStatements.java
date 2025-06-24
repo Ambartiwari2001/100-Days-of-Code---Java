8a60f31926c6239c36fa3f775f722bd09ab5f41e
Ambar Tiwari <ambartiwariji1995@gmail.com>
2025-06-24 09:17:10 +0530

    Day 4: Added Java control statements examples (if-else, switch, loops)

---
+++ Day04/ControlStatements.java
@@ -0,0 +1,95 @@
+package Day04;
+
+import java.util.Scanner;
+
+public class ControlStatements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+
+        // Call next method
+        gradeChecker();
+    }
+
+    public static void gradeChecker() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your marks: ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("Grade: A");
+        } else if (marks >= 75) {
+            System.out.println("Grade: B");
+        } else if (marks >= 50) {
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Grade: Fail");
+        }
+
+        // Call next method
+        numberToDay();
+    }
+
+    public static void numberToDay() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter day number (1-7): ");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day number");
+        }
+
+        loopExample();
+    }
+
+    public static void loopExample() {
+        System.out.println("Numbers from 1 to 10 using for loop:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n\nEven numbers between 1 to 20 using while loop:");
+        int i = 2;
+        while (i <= 20) {
+            System.out.print(i + " ");
+            i += 2;
+        }
+
+        System.out.println("\n\nCountdown using do-while loop:");
+        int j = 5;
+        do {
+            System.out.println("Countdown: " + j);
+            j--;
+        } while (j >= 1);
+    }
+}
