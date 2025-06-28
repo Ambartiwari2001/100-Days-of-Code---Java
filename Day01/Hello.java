97c518c834e7020153f80f3dc0860e00f590b19a
Ambar Tiwari <ambartiwariji1995@gmail.com>
2025-06-28 09:56:25 +0530

    Day01: Completed basic Java programs

--- Day01/Hello.java
+++ Day01/Hello.java
@@ -2,6 +2,6 @@ package Day01;
 
 public class Hello {
     public static void main(String args[]) {
-        System.out.print("Hello World !");
+        System.out.print("Hello World !"); 
     }
 }
