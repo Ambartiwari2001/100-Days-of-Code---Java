4e506d01848b96ef2a06fd02b81911d8bbcfc43c
Ambar Tiwari <ambartiwariji1995@gmail.com>
2025-06-28 09:57:47 +0530

    Day02: Added Even/Odd number checker program

--- Day02/EvenOdd.java
+++ Day02/EvenOdd.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 class EvenOdd{
     public static void main(String[] args)
-    {
+    { 
       Scanner sc = new Scanner(System.in);
       System.out.print("Enter the number: ");
       int num = sc.nextInt();
