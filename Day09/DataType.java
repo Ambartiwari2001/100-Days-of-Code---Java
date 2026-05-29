a739b9b1b504a0b09e8f07feb9d5422aacc8b746
Ambar Tiwari <ambartiwariji1995@gmail.com>
2026-05-29 08:39:09 +0530

    add Pascal-Tree program using array

---
+++ Day09/DataType.java
@@ -0,0 +1,9 @@
+// in java datatype is a type of data there are two 
+// types of datatypes first is primitive and second is not-primitive
+// in this code i'm working with primitive data types.
+class DataType {
+    public static void main(String[] args) {
+        float marks = 4.2f;
+        System.out.println(marks);
+    }
+}
